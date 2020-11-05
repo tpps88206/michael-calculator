8e43fa857c595fda61f48aa1f023142022b3a86b
Sheng-Chuan Hsu <cool96482@hotmail.com>
2020-11-05 13:04:52 +0800

    change text

--- src/pages/Calculator/index.js
+++ src/pages/Calculator/index.js
@@ -7,7 +7,8 @@ import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Dialog from '@material-ui/core/Dialog';
-import GradientIcon from '@material-ui/icons/Gradient';
+import CancelPresentationIcon from '@material-ui/icons/CancelPresentation';
+import OpenInNewIcon from '@material-ui/icons/OpenInNew';
 import { makeStyles } from '@material-ui/styles';
 
 import logoImg from '../../assets/logo.png';
@@ -193,10 +194,10 @@ const CalculatorPage = () => {
             variant="contained"
             color="secondary"
             size="large"
-            startIcon={<GradientIcon />}
+            startIcon={open ? <CancelPresentationIcon /> : <OpenInNewIcon />}
             onClick={handleClickOpen}
           >
-            點擊打開計算機
+            {open ? <p>點擊計算機以外關閉</p> : <p>點擊打開計算機</p>}
           </Button>
         </div>
         {ContentComponent()}
